b7d6f1e8172c5669e5ae64b9614223b59dd97383
Lionqueen94 <lionqueen94@gmail.com>
2025-06-19 11:40:57 +0200

    Fixed queries and added committee session creation to election import

--- frontend/src/types/generated/openapi.ts
+++ frontend/src/types/generated/openapi.ts
@@ -338,10 +338,10 @@ export interface ClaimDataEntryResponse {
 export interface CommitteeSession {
   election_id: number;
   id: number;
-  location: string;
+  location?: string | null;
   number: number;
-  start_date: string;
-  start_time: string;
+  start_date?: string | null;
+  start_time?: string | null;
   status: CommitteeSessionStatus;
 }
 
@@ -356,10 +356,10 @@ export interface CommitteeSessionCreateRequest {
 export interface CommitteeSessionDetails {
   sessionElectionId: number;
   sessionId: number;
-  sessionLocation: string;
+  sessionLocation?: string | null;
   sessionNumber: number;
-  sessionStartDate: string;
-  sessionStartTime: string;
+  sessionStartDate?: string | null;
+  sessionStartTime?: string | null;
   sessionStatus: string;
 }
 
@@ -373,7 +373,7 @@ export interface CommitteeSessionListResponse {
 /**
  * Committee session status
  */
-export type CommitteeSessionStatus = "Created" | "DataEntryInProgress" | "DataEntryPaused" | "DataEntryFinished";
+export type CommitteeSessionStatus = "created" | "data_entry_in_progress" | "data_entry_paused" | "data_entry_finished";
 
 /**
  * Committee session update request
